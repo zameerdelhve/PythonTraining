14a31e9ea027189594a88a5a1df3646d020754cd
zameerdelhve <zameerdelhve@gmail.com>
2021-05-22 13:50:12 +0530

    1=2

--- BuyApp.py
+++ BuyApp.py
@@ -1,7 +1,7 @@
 credit = input('Is buyer credit good? ')
 credit_score = credit.upper()
 print(credit_score)
-if credit_score == "YES" and 1==2:
+if credit_score == "YES" and 1==3:
     print("good credit")
 elif credit_score == "NO":
     print("bad credit")
