19fe57ac1d4e9d03b081ad05ac2f37e2ecf100f3
zameerdelhve <zameerdelhve@gmail.com>
2021-05-23 20:10:06 +0530

    while practice

--- ClassApp.py
+++ ClassApp.py
@@ -1,17 +1,17 @@
+import BuyApp
 class Person:
-    def __init__(self,First_Name, Last_Name):
+    def __init__(self, First_Name, Last_Name):
         self.First_Name = First_Name
         self.Last_Name = Last_Name
+
     def talk(self):
-        print ("Hell0 " + self.First_Name + ' ' + self.Last_Name)
+        print("Hell0 " + self.First_Name + ' ' + self.Last_Name)
 
 
-
-
+BuyApp.credit_score
 person = Person("Zameer", "Delhve")
 person.talk()
 
 person1 = Person("Alifya", "Faizee")
 person1.talk()
-#print(person.First_Name + ' ' + person.Last_Name)
-
+# print(person.First_Name + ' ' + person.Last_Name)
---
+++ DiceApp.py
@@ -0,0 +1,11 @@
+import random
+class Dice:
+    def roll(self):
+        x = random.randint(1,6)
+        y = random.randint(1,6)
+        roll_of_dice = (x,y)
+        return roll_of_dice
+
+dice = Dice()
+#dice.roll()
+print(dice.roll())
