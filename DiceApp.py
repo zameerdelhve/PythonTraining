3373315334a56416bae42a5df1b2b97fa18ecf15
zameerdelhve <zameerdelhve@gmail.com>
2021-05-23 20:13:24 +0530

    while practice

--- DiceApp.py
+++ DiceApp.py
@@ -1,11 +1,13 @@
 import random
+
+
 class Dice:
     def roll(self):
-        x = random.randint(1,6)
-        y = random.randint(1,6)
-        roll_of_dice = (x,y)
+        x = random.randint(1, 6)
+        y = random.randint(1, 6)
+        roll_of_dice = (x, y)
         return roll_of_dice
 
 dice = Dice()
-#dice.roll()
+# dice.roll()
 print(dice.roll())
