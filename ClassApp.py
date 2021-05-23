e73ee7f05eded6411c8fd7cf7bad917e69e144ac
zameerdelhve <zameerdelhve@gmail.com>
2021-05-23 19:16:26 +0530

    while practice

--- ClassApp.py
+++ ClassApp.py
@@ -1,13 +1,14 @@
 class Person:
-    def talk(self):
-        print ("Hell0")
-
-
     def __init__(self,First_Name, Last_Name):
         self.First_Name = First_Name
         self.Last_Name = Last_Name
+    def talk(self):
+        print ("Hell0 " + self.First_Name + ' ' + self.Last_Name)
+
+
+
 
 person = Person("Zameer", "Delhve")
 person.talk()
-print(person.First_Name + ' ' + person.Last_Name)
+#print(person.First_Name + ' ' + person.Last_Name)
 
